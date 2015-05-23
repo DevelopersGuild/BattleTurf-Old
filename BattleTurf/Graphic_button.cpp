bcda6d0d58858739dc6ae5ddd55632b8d0e52f5e
Hendry Joe <hendryjoe.user@gmail.com>
2015-05-22 18:22:22 -0700

    Modify Graphic_button class

--- BattleTurf/Graphic_button.cpp
+++ BattleTurf/Graphic_button.cpp
@@ -2,16 +2,45 @@
 
 Graphic_button::Graphic_button()
 {
-
 }
 
-Graphic_button::Graphic_button(sf::Font *font)
-{
+Graphic_button::Graphic_button(sf::Font *font){
     fontptr = font;
 }
 
+
+Graphic_button::Graphic_button(float length, float height, int x, int y)
+{
+    /* JUST TO TEST THE FONT */
+    sf::Font *font  = new sf::Font;
+    font->loadFromFile("arial.ttf");
+    fontptr = font;
+    ///////////////
+    
+    this->setPosition(xPos, yPos);
+    this->setFillColor(sf::Color::Blue);
+    this->setSize(sf::Vector2f(length, height));
+    
+    //TO TEST THE TEXT, MODIFY LATER
+    text.setCharacterSize(24);
+    text.setString("ASDASD");
+    text.setColor(sf::Color::Black);
+    text.setPosition(xPos+10, yPos+10);
+    text.setFont(*fontptr);
+    ///////
+}
+
+
 bool Graphic_button::isCursor_On_button(const sf::Vector2i &mouseposition)
 {
-    return true;
-    //
+    if (mouseposition.x >= xPos && mouseposition.x <= xPos+length &&
+        mouseposition.y >= yPos && mouseposition.y <= yPos+height){
+        return true;
+    }
+    return false;
+}
+
+void Graphic_button::addInto(sf::RenderWindow* windowPtr){
+    windowPtr->draw(*this);
+    windowPtr->draw(text);
 }
--- BattleTurf/Graphic_button.h
+++ BattleTurf/Graphic_button.h
@@ -6,6 +6,10 @@
 class Graphic_button : public sf::RectangleShape
 {
 private:
+    float length;
+    float height;
+    int xPos;
+    int yPos;
     sf::Font *fontptr;
     sf::Text text;  //text on the button
     sf::Vector2i position;
@@ -13,9 +17,11 @@ private:
 public:
     Graphic_button();
     Graphic_button(sf::Font *font);
+    Graphic_button(float length, float height, int x, int y);
     void setString(sf::String str){text.setString(str);}
 
     bool isCursor_On_button(const sf::Vector2i &mouseposition);
+    void addInto(sf::RenderWindow* windowPtr);
 };
 
 #endif // GRAPHIC_BUTTON_H_INCLUDED
